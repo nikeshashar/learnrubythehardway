9d993327cb269f5114e83d309c37aa4c2277a6f0
Nikesh Ashar <nikeshashar@gmail.com>
2016-07-20 13:28:12 +0100

    Ex11 completed

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+puts "How old are you?"
+age = gets.chomp
+puts "How tall are you?"
+height = gets.chomp
+puts "How heavy are you?"
+weight = gets.chomp
+
+puts "So, you're #{age} years old, #{height}cm tall and #{weight} heavy"
